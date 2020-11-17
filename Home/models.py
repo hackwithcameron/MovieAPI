fc9a81bb527163db009d817336909103d7c7f196
Cameron <hackwithcameron@gmail.com>
2020-11-16 16:33:40 -0800

    set up like and dislike buttons

--- Home/models.py
+++ Home/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100, blank=False)
-    id = models.IntegerField(blank=False, primary_key=True)
-    thumbsUp = models.IntegerField()
+    id = models.CharField(blank=False, primary_key=True, max_length=10)
+    thumbsUp = models.IntegerField(default=0)
+    thumbsDown = models.IntegerField(default=0)
 
     Movies = models.Manager()
 
--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import SearchForm
+from .models import Movie
 from .api_service import search_title, get_details
 
 
@@ -8,12 +9,25 @@ def index(request):
     context = {
         'movie': movie,
         'results': [],
+        'rating': []
     }
     if request.method == 'POST':
-        search_results = api_request(request)
-        for i in search_results:
-            context['results'].append(i)
-        return render(request, 'Home/home.html', context)
+        if 'search_movies' in request.POST:
+            search_results = api_request(request)
+            for i in search_results:
+                print(get_ratings(i['id']))
+                if get_ratings(i['id']):
+                    rating = get_object_or_404(Movie, pk=i['id'])
+                    print(rating)
+                context['results'].append(i)
+            return render(request, 'Home/home.html', context)
+        elif 'like' in request.POST:
+            print(request.POST.get('like'))  # prints id of movie button clicked
+
+
+
+            return redirect('Home:index')
+
     return render(request, 'Home/home.html', context)
 
 
@@ -29,3 +43,23 @@ def api_request(request):
     title = request.POST.get('search_movie', False)
     res = search_title(title)
     return res
+
+
+def get_ratings(movie_id):
+    movie = Movie.Movies.filter(pk=movie_id)
+    if movie:
+        rating = {
+            'likes': movie.thumbsUp,
+            'dislikes': movie.thumbsDown
+        }
+        return rating
+    else:
+        return False
+
+
+def post_like():
+    pass
+
+
+def post_dislike():
+    pass
